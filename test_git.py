8f6b76fb2c29702678e5c3d2007c51652ee93ecb
Saif1005 <saifakkari.sa@gmail.com>
2025-10-07 13:36:38 +0100

    saif version 1

---
+++ test_git.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import pandas as pd 
+
+
+class Calcul : 
+    def __init__(self , num1 , num2 ) :
+
+        self.num1 = num1 
+        self.num2 = num2
+
+
+    def add(self) : 
+        
+
+        return self.num2 + self.num1 
+    
+
+def main(self) : 
+    
+    result = Calcul()
+    num1 = 1 
+    num2 = 3 
+
+    return result.add(num1 ,   num2 )
+
+
+main()
